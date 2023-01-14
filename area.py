dddb40552088d8de27401e269398a019482bec95
AanisaSarthar <122104032+AanisaSarthar@users.noreply.github.com>
2023-01-14 18:14:20 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,4 @@
+pi= 3.14  
+Radius = float (input ("Input the radius of the circle: "))  
+area_of_the_circle = pi * Radius * Radius  
+print (" The area of the circle with radius:1.1 ", area_of_the_circle)  
